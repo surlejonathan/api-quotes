ef289e6c371258e6287f1c122f5c01815554ec7f
surlejonathan <surle.jonathan@gmail.com>
2020-10-21 22:33:08 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React from 'react';
 import './App.css';
 import QuoteCard from './Components/QuoteCard';
-import axios from 'axios';
 
-const cardDefault = {
-  character: "Apu Nahasapeemapetilon",
-  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
-  quote: "By chilling my loins I increase the chances of impregnating my wife.",
+const App = () => {
+  return (
+    <div>
+      <QuoteCard/>
+
+
+    </div>
+  );
 }
 
-class App extends React.Component{
-  constructor(props) {
-  super(props);
-    this.state = {
-      card : cardDefault,
-    }
-  }
-
-  changeQuote = () => {
-    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
-      .then(responses => responses.data)
-      .then(data => this.setState({
-          card : data[0]
-      }))
-  }
-
-  render(){
-    return (
-      <div>
-        <QuoteCard card={this.state.card}/>
-        
-        <button onClick={() => this.changeQuote()}>Change quote</button>
-      </div>
-    );
-  }
-}
 
 export default App;
--- src/Components/QuoteCard.js
+++ src/Components/QuoteCard.js
@@ -1,27 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../style/QuoteCard.css';
+import axios from 'axios';
 
+const defaultCard = {
+    character: "Apu Nahasapeemapetilon",
+    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+    quote: "By chilling my loins I increase the chances of impregnating my wife.",
+}
 
+class QuoteCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            card: defaultCard,
+            loading : false,
+        }
+    }
 
-const QuoteCard = ({card}) => {
+    changeQuote = () => {
 
-    return (
-        <figure className="QuoteCard">
-            <img
-                src={card.image}
-                alt={card.character} />
-            <figcaption>
-                <blockquote>
-                    {card.quote}
-                </blockquote>
-                <p>
-                    <cite>{card.character}</cite>
-                </p>
-            </figcaption>
-            
-        </figure>
-    )
+        axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+            .then(responses => responses.data)
+            .then(data => this.setState({
+                card: data[0]
+            }))
+    }
+
+    render() {
+
+        const { image, character, quote } = this.state.card;
+
+        return (
+            <div>
+                <figure className="QuoteCard">
+                    <img
+                        src={image}
+                        alt={character} />
+                    <figcaption>
+                        <blockquote>
+                            {quote}
+                        </blockquote>
+                        <p>
+                            <cite>{character}</cite>
+                        </p>
+                    </figcaption>
+                </figure>
+                <button onClick={() => this.changeQuote()}>Change quote</button>
+            </div>
+        )
+    }
+
 }
 
 
